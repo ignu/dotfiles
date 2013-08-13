08831e7d1f650dfbc6ab60a892715ffdf665202b
Len Smith <len@barrison.com>
2013-08-13 19:02:55 -0400

    Added minimum install script for linux

---
+++ min-install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "installing oh my zsh"
+  wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+  cd ~/
+
+echo "cloning repository"
+  mkdir ~/bin
+  cd ~/bin
+  git clone git@github.com:ignu/dotfiles2.0.git
+
+echo "linking files"
+  cd
+  ln ~/bin/dotfiles2.0/ackrc .ackrc
+  ln ~/bin/dotfiles2.0/vimrc .vimrc
+  ln ~/bin/dotfiles2.0/gvimrc .gvimrc
+  ln -s ~/bin/dotfiles2.0/vim .vim
+  ln ~/bin/dotfiles2.0/gitconfig .gitconfig
+  ln ~/bin/dotfiles2.0/aliasrc .aliasrc
+  ln ~/bin/dotfiles2.0/tmux.conf .tmux.conf
+
+  rm .zshrc
+  ln ~/bin/dotfiles2.0/zshrc .zshrc
+  rm -rf ~/.zsh_profile.d
+  ln -s ~/bin/dotfiles2.0/zsh_profile.d .zsh_profile.d
+
+echo "updating vim packages"  
+  mkdir .vim/bundle
+  .vim/update_bundles
