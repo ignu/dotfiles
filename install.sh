11004a9f17c6183353ffec7a461433eba309b51c
Len Smith <len@barrison.com>
2017-05-24 08:35:49 -0700

    Update install script

--- install.sh
+++ install.sh
@@ -15,28 +15,15 @@ echo "linking files"
   ln ~/bin/dotfiles2.0/vimrc .vimrc
   ln ~/bin/dotfiles2.0/gvimrc .gvimrc
   ln -s ~/bin/dotfiles2.0/vim .vim
+  ln -s ~/bin/dotfiles2.0/vim ~/config/nvim
   ln ~/bin/dotfiles2.0/gitconfig .gitconfig
   ln ~/bin/dotfiles2.0/aliasrc .aliasrc
   ln ~/bin/dotfiles2.0/tmux.conf .tmux.conf
-
-  ln ~/bin/dotfiles2.0/spacemacs .spacemacs
-  ln -s ~/bin/dotfiles2.0/emacs.d/ .emacs.d/private
 
   rm .zshrc
   ln ~/bin/dotfiles2.0/zshrc .zshrc
   rm -rf ~/.zsh_profile.d
   ln -s ~/bin/dotfiles2.0/zsh_profile.d .zsh_profile.d
 
-echo "updating vim packages"  
-  mkdir .vim/bundle
-  .vim/update_bundles
-
-echo "setup postgres"
-  initdb /usr/local/var/postgres
-  cp /usr/local/Cellar/postgresql/9.0.1/org.postgresql.postgres.plist ~/Library/LaunchAgents
-  launchctl load -w ~/Library/LaunchAgents/org.postgresql.postgres.plist
-  pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
-
 echo "install pow"
   curl get.pow.cx | sh
- 
