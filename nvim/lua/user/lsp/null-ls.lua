f354f2a80ce77dff29cd0018a4cf9f1adf54dab8
Len Smith <len@barrison.com>
2022-04-10 16:55:16 -0700

    Vim format on save/root project in monorepos

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -8,12 +8,55 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+-- format async
+_G.formatting_async = function(bufnr)
+    bufnr = tonumber(bufnr) or vim.api.nvim_get_current_buf()
+
+    vim.lsp.buf_request(
+        bufnr,
+        "textDocument/formatting",
+        { textDocument = { uri = vim.uri_from_bufnr(bufnr) } },
+        function(err, res)
+            if err then
+                local err_msg = type(err) == "string" and err or err.message
+                -- you can modify the log message / level (or ignore it completely)
+                vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+                return
+            end
+
+            -- don't apply results if buffer is unloaded or has been modified
+            if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+                return
+            end
+
+            if res then
+                vim.lsp.util.apply_text_edits(res, bufnr)
+                vim.api.nvim_buf_call(bufnr, function()
+                    vim.cmd("silent noautocmd update")
+                end)
+            end
+        end
+    )
+end
+
+
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier,
 		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
+   	formatting.stylua,
     -- diagnostics.flake8
 	},
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            -- wrap in an augroup to prevent duplicate autocmds
+            vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePost <buffer> lua formatting_async(vim.fn.expand("<abuf>"))
+            augroup END
+            ]])
+        end
+    end,
 })
